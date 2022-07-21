c9d0469468651f5ad81cae97cb0fafdda5c302d7
Olga Antushevich <antushevich@gmail.com>
2022-07-21 17:28:23 +0300

    feat: implement functions for section 04-date-tasks.js (without 2 functions)

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
 
 
@@ -53,8 +53,10 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const d = new Date(date);
+  const year = d.getFullYear();
+  return new Date(year, 1, 29).getDate() === 29;
 }
 
 
@@ -95,6 +97,11 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(/* date */) {
+  // const hours = date.getUTCHours();
+  // const minutes = date.getUTCMinutes();
+  // const seconds = date.getUTCSeconds();
+  // const angle = ((hours * 30 + minutes * 0.5 + seconds * 0.008333333333333333) * Math.PI) / 180;
+  // return angle;
   throw new Error('Not implemented');
 }
 
